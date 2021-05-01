a11617db989c76519cba2472a1b123522fad6d05
veritem <mugaboverite@gmail.com>
2021-05-02 00:53:57 +0200

    feat: init

---
+++ src/routes/about.svelte
@@ -0,0 +1,20 @@
+<h1>About us here</h1>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod illo dolore deleniti debitis ut
+	molestiae tempore perspiciatis consequuntur, architecto cupiditate minus sapiente ab mollitia
+	praesentium voluptatum amet voluptates officiis! Debitis eveniet consectetur quasi aut facilis
+	deleniti autem. Suscipit fuga quam laboriosam ab sit architecto veritatis molestias commodi minus
+	tempora pariatur voluptatem perspiciatis, quasi voluptatibus qui aspernatur quod quo repellendus
+	odit eveniet sunt provident quia aliquid. Culpa voluptates incidunt rerum ad laudantium
+	consectetur similique, mollitia magnam impedit molestiae ratione vero quae eos voluptatibus cum
+	voluptate optio deleniti eum. Perferendis, minima esse laboriosam illo non, deleniti sint
+	doloribus dignissimos, explicabo eligendi expedita quibusdam repellat aut! Ex ab odio tempore
+	temporibus iure suscipit illum et perspiciatis adipisci vero voluptatum eos obcaecati optio, ipsam
+	accusantium nobis excepturi, natus nisi sequi dolores veritatis dolor saepe, deleniti aspernatur!
+	Quis vitae recusandae delectus commodi! Natus ea dicta corporis harum, sapiente, vel veritatis
+	officiis debitis in, quia dignissimos quidem doloribus quasi mollitia magnam numquam perferendis
+	quam repellendus ullam excepturi laboriosam ratione velit! Fugiat, consequatur voluptatibus non
+	cumque ea nulla doloremque. Corrupti iusto illo doloribus fugit ullam impedit, dolorem minima
+	dolore perferendis, dolorum quidem maiores neque odio. Distinctio labore animi non ipsum modi,
+	assumenda ipsa nesciunt reiciendis ducimus id!
+</p>
---
+++ src/routes/team.svelte
@@ -0,0 +1,20 @@
+<h1>Team members</h1>
+<p>
+	Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum dolores facere ipsa fugiat
+	voluptates perferendis quaerat doloribus, quidem incidunt quasi animi dolorum mollitia cum sequi
+	velit pariatur dolorem, reiciendis perspiciatis nulla exercitationem quis. Repudiandae eos omnis
+	nobis dolorum culpa cumque odio hic excepturi quibusdam ipsa illum ea corrupti exercitationem
+	nesciunt accusantium ullam esse, maxime delectus. Consequuntur eius laborum similique est, labore
+	vero ipsum repellendus praesentium suscipit necessitatibus distinctio sint asperiores eum
+	provident iste dolorum placeat cumque maxime alias fugit ea aspernatur inventore quia quae!
+	Temporibus, eaque minima. Ipsam necessitatibus, aliquam quos eaque similique unde officia iure
+	quaerat ea nemo molestiae dignissimos rem facilis sunt, minima, ducimus voluptatibus hic! Quas,
+	quod ex nemo veritatis obcaecati dolorem, consequatur neque iusto explicabo sequi velit ab veniam
+	asperiores iste odio fuga provident fugit at perspiciatis sed reiciendis magnam debitis rerum?
+	Veniam aliquid, illum sint quidem facilis nobis, quisquam tempore saepe fugit molestias dolorem ad
+	quod, eligendi dolorum? Quas sit inventore earum! Facere, maiores repellat. Amet, facilis quae
+	dolor explicabo, illum perspiciatis enim consequuntur ipsa mollitia architecto numquam repellat
+	laudantium dicta iure quasi dolorem excepturi. Laboriosam, ea harum veniam nostrum dolorum, itaque
+	obcaecati quisquam, odio officia sequi perspiciatis et velit cumque vitae praesentium quia
+	recusandae.
+</p>
