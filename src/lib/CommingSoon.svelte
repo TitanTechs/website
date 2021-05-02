490ecbefedac129e57bf4001202357c961a55c74
Didien Munezero <51176044+didiermunezero@users.noreply.github.com>
2021-05-02 15:48:01 +0900

    Update CommingSoon.svelte

--- src/lib/CommingSoon.svelte
+++ src/lib/CommingSoon.svelte
@@ -4,6 +4,6 @@
 
 <Title title="Titan Technologies inc" />
 <div class="text-center">
-	<h1 class="text-4xl">Comming soon ....</h1>
+	<h1 class="text-4xl">Coming soon ....</h1>
 	<h3>Titans Technologies inc</h3>
 </div>
